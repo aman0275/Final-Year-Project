00c52ac014c9d13f859a4055dfbc87e22b1bbcd8
Aman Sharma <aman0275@gmail.com>
2022-01-13 21:33:49 +0530

    template created

--- imajoy/imajoy/views.py
+++ imajoy/imajoy/views.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("hello")
+    params = {'name':'Aman','place':'Jab'}
+    return render(request,'index.html',params)
 def about(request):
     return HttpResponse("hello about")
+
+    
