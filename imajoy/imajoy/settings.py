3e3ebf98a11757d78fd115d378bef0ec276f7fca
Aman Sharma <aman0275@gmail.com>
2022-01-13 21:23:08 +0530

    views created

--- imajoy/imajoy/settings.py
+++ imajoy/imajoy/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'imajoy.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
