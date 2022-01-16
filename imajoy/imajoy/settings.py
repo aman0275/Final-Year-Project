f677d397aeeee2feb352052b56115b7036e2c6d1
Aman Sharma <aman0275@gmail.com>
2022-01-16 21:14:41 +0530

    tailwind added

--- imajoy/imajoy/settings.py
+++ imajoy/imajoy/settings.py
@@ -37,8 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tailwind',
     'imageprocess.apps.ImageprocessConfig'
 ]
+
+TAILWIND_APP_NAME = 'imageprocess'
+
+
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
