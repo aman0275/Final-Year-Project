949b0d84d45768712ea7eade6627186ce2b43a83
Aman Sharma <aman0275@gmail.com>
2022-06-19 16:10:57 +0530

    image upload working

--- imajoy/imageprocess/views.py
+++ imajoy/imageprocess/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import UploadImage
 from imageprocess.forms import UserImage
-from .load import run
+from .process import run
 
 
 # Create your views here.
@@ -12,18 +12,17 @@ def home(request):
         form = UserImage(request.POST, request.FILES)  
         if form.is_valid():  
             form.save()  
-  
             # Getting the current instance object to display in the template  
             img_object = form.instance  
-              
+            print(img_object.image.url)
+            run(img_object.image.url)
             return render(request, 'imageprocess/index.html', {'form': form, 'img_obj': img_object})  
     else:  
         form = UserImage()  
   
-    
+
     return render(request,'imageprocess/index.html',{'form':form})
 
 
 def test(request):
-        run()
         return HttpResponse("testing")
